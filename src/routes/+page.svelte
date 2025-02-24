c7fdb8aa5acfe0442cd9c839623596835e362235
hlavkat <hlavkat@gmail.com>
2025-02-24 14:11:30 +0100

    added githublink

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -185,8 +185,8 @@
 <Divider --double-padding="0" />
 <footer>
 	<span>hello@snaha.net</span>
-	<a href="/">Contact us</a>
-	<a href="/">Github</a>
+	<a href="mailto:hello@snaha.net">Contact us</a>
+	<a href="https://github.com/snaha" target="_blank">Github</a>
 	<a href="/">Discord</a>
 	{#if !isMobile}
 		<div class="grower"></div>
