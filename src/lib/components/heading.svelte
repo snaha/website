12f7554686f0fdab19a58934de3672aa98c37769
hlavkat <hlavkat@gmail.com>
2025-02-27 10:19:39 +0100

    fix: spacing between tranformed titles and other objects

--- src/lib/components/heading.svelte
+++ src/lib/components/heading.svelte
@@ -1,20 +1,42 @@
 <script lang="ts">
+	import { onMount } from 'svelte'
+
 	interface Props {
 		text: string
 		color?: 'accent' | 'top'
 		large?: boolean
 	}
 	let { text, color = 'top', large = false }: Props = $props()
+
+	let root: HTMLDivElement
+	let wrapper: HTMLDivElement
+
+	function setRootHeight() {
+		const rect = wrapper.getBoundingClientRect()
+		root.style.height = `${rect.height}px`
+	}
+	onMount(() => {
+		window.addEventListener('resize', setRootHeight)
+		setRootHeight()
+		return () => window.removeEventListener('resize', setRootHeight)
+	})
 </script>
 
-<div class="root {color}" class:large>
-	{#each text.split(' ') as word}
-		<span>{word}</span>
-	{/each}
+<div class="root" bind:this={root}>
+	<div class="wrapper {color}" class:large bind:this={wrapper}>
+		{#each text.split(' ') as word}
+			<span>{word}</span>
+		{/each}
+	</div>
 </div>
 
 <style>
 	.root {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.wrapper {
 		display: flex;
 		flex-direction: column;
 		font-family: var(--font-family-snaha);
@@ -26,6 +48,7 @@
 		transform: rotate(-11deg) skewX(-11deg);
 		width: fit-content;
 	}
+
 	.accent {
 		color: var(--colors-high-accent);
 	}
