73a5a1ad0c682264961a9367d11369a6520fd0f5
hlavkat <hlavkat@gmail.com>
2025-02-28 14:13:01 +0100

    feat: social link preview

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,22 @@
 <script>
+	import { base } from '$app/paths'
 	import '../app.css'
 	import '../diete.css'
 
 	const { children } = $props()
 </script>
 
+<svelte:head>
+	<meta property="og:title" content="Snaha Collective" />
+	<meta
+		property="og:description"
+		content="An OG team geared towards making the web a bit better for hoomans."
+	/>
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content={`${base}/og_image.png`} />
+	<meta property="og:url" content="https://snaha.net" />
+	<meta name="twitter:card" content={`${base}/og_image.png`} />
+</svelte:head>
 <main>
 	{#if children}
 		{@render children()}
