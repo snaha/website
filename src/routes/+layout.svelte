0c9a2c9c9c7363b44e1a18d627a28d2fe86620df
hlavkat <hlavkat@gmail.com>
2025-02-28 08:15:58 +0100

    feat: favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,11 @@
 	const { children } = $props()
 </script>
 
+<svelte:head>
+	<link rel="icon" href="/favicon.ico" sizes="32x32" />
+	<link rel="icon" href="/icon.svg" type="image/svg+xml" />
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+</svelte:head>
 <main>
 	{#if children}
 		{@render children()}
