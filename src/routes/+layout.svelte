f77d9dc057db0768d390970e70ef4953e3b14a7b
Attila Gazso <230163+agazso@users.noreply.github.com>
2025-06-04 10:32:47 +0200

    feat: analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,24 @@
 <script>
+	import { page } from '$app/state'
 	import LinkPreview from '$lib/components/link-preview.svelte'
 	import '../app.css'
 	import '../diete.css'
 
 	const { children } = $props()
+
+	function isProductionEnvironment() {
+		return page.url.origin === 'https://snaha.net'
+	}
 </script>
 
 <svelte:head>
+	{#if isProductionEnvironment()}
+		<script
+			defer
+			src="https://cloud.umami.is/script.js"
+			data-website-id="0e254067-63e9-43d3-a94e-bff5dc2b6005"
+		></script>
+	{/if}
 	<LinkPreview />
 </svelte:head>
 
